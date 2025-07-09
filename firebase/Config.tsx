f9b6e9b775f7002af775ce64696ec186ac58ff19
luiscr918 <lc9107040@gmail.com>
2025-07-09 08:33:35 -0500

    corregido envio de datos importantes por archivo secreto

--- firebase/Config.tsx
+++ firebase/Config.tsx
@@ -1,18 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+//importar credenciales
+import * as credenciales from "../secrets/credenciales";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD8Un4qe1SEzIjwPDtAJdLK0WgjBU-An-A",
-  authDomain: "app-fire-394da.firebaseapp.com",
-  databaseURL: "https://app-fire-394da-default-rtdb.firebaseio.com",
-  projectId: "app-fire-394da",
-  storageBucket: "app-fire-394da.firebasestorage.app",
-  messagingSenderId: "558930400943",
-  appId: "1:558930400943:web:f2d0a873edcc1f35b8e65a"
+  apiKey: credenciales.apiKey,
+  authDomain: credenciales.authDomain,
+  databaseURL: credenciales.databaseURL,
+  projectId: credenciales.projectId,
+  storageBucket: credenciales.storageBucket,
+  messagingSenderId: credenciales.messagingSenderId,
+  appId: credenciales.appId,
 };
 
 // Initialize Firebase
